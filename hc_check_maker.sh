064e8b45710f13aa578c00652a4af71ac726eb98
Bo <bo@saurage.se>
2020-11-05 01:36:49 +0100

    2

--- hc_check_maker.sh
+++ hc_check_maker.sh
@@ -24,7 +24,7 @@ PAYLOAD='{"name": "'`hostname`'", "timeout": 60, "grace": 300, "unique": ["name"
 # Create the check if it does not exist.
 # Grab the ping_url from JSON response using the jq utility:
 URL=`curl -s https://healthchecks.io/api/v1/checks/  -H "X-Api-Key: $API_KEY" -d "$PAYLOAD"  | jq -r .ping_url`
-URL_WWW=https://healthchecks.io/checks/${URL##*/}
+URL_WWW=https://healthchecks.io/checks/${URL##*/}/details/
 
 echo $URL
 
@@ -64,8 +64,6 @@ EOF
 # Load the job
 launchctl load -w $HOME/Library/LaunchAgents/local.healthchecks_service.plist
 
-
-
 # Open the checks webpage
-open $URLWWW
+open $URL_WWW
 exit 0
